196cefcc48d62749c8ecc4fd4be24e4eab5a059a
roger10e <rogertenny@gmail.com>
2022-04-23 14:06:41 -0500

    planning steps outlined

---
+++ script.js
@@ -0,0 +1,32 @@
+//REQUIREMENTS:
+//GIVEN a command-line application that accepts user input
+//WHEN I am prompted for information about my application repository
+//THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+//WHEN I enter my project title
+//THEN this is displayed as the title of the README
+//WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+//THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+//WHEN I choose a license for my application from a list of options
+//THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+//WHEN I enter my GitHub username
+//THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+//WHEN I enter my email address
+//THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+//WHEN I click on the links in the Table of Contents
+//THEN I am taken to the corresponding section of the README
+
+//GETTING STARTED:
+//create .gitignore file & include node_modules/ and .DS_Store/ so that 
+    //node_modules isn't tracked or uploaded to github
+    //create .gitignore file before installing any npm dependencies
+//Make sure repo includes package.json with required dependencies
+    //Create one by running npm init when you set up the project
+//Include a video of the typical user flow through application - how?
+    //2.2.4 Pre-Work Module - ScreenCastify Tutorial
+    //Submit a link to the video and add it to the ReadMe
+//Include other screenshots necessary for introduction to our application
+
+//How to set up  file structure
+//How to properly structure .gitignore
+//How to connect to node and to the nnpm I need
+    //Inquirer?
